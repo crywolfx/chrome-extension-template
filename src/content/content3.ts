a855c0252c317d0aad63a795ec329c15fc7002da
wangxin20 <wangxin20@kuaishou.com>
2023-11-24 17:14:52 +0800

    upd: request

--- src/content/content3.ts
+++ src/content/content3.ts
@@ -1,3 +1,5 @@
-export {};
-console.log('only js');
-console.log('window', window);
+import request from 'src/lib/request';
+
+request.get('https://www.baidu.com').then((res) => {
+  console.log(res);
+})
